5cab2b1ff7cb537865e1bbf4b9212deae19531aa
K-116005054 <61305320+K-116005054@users.noreply.github.com>
2020-02-24 13:55:19 +0530

    Update monopoly_old.js

--- monopoly_old.js
+++ monopoly_old.js
@@ -1,105 +1,49 @@
- var box1 = document.querySelector('#box1');
- var box2 = document.querySelector('#box2');
- var box3 = document.querySelector('#box3');
- var box4 = document.querySelector('#box4');
- var box5 = document.querySelector('#box5');
- var box6 = document.querySelector('#box6');
- var box7 = document.querySelector('#box7');
- var box8 = document.querySelector('#box8');
- var box9 = document.querySelector('#box9');
- var box10 = document.querySelector('#box10');
- var box11 = document.querySelector('#box11');
- var box12 = document.querySelector('#box12');
- var box13 = document.querySelector('#box13');
- var box14 = document.querySelector('#box14');
- var box15 = document.querySelector('#box15');
- var box16 = document.querySelector('#box16');
-
-var imageArray = [box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16];
-const board = [];
-for(var i = 0; i<16;i++)
-{
-    board[i] = i*10;
+class player{
+    constructor(name,position,money)
+    {
+        this.name = name;
+        this.position = position;
+        this.money = money;
+    }
+    rollDice(player_image)
+    {
+        let pos = Math.floor(Math.random()*6)+1;
+        console.log(pos);
+        this.updatePosition(pos);
+    }
+    updatePosition(pos)
+    {
+        this.position += pos;
+        console.log(this.position);
+        console.log("position",this.position);
+        this.updateMoney();
+    }
+    updateMoney()
+    {   
+        if(this.position<board.length)
+        this.money -= board[this.position];
+        else
+        {
+            this.position %= 15;
+            this.money -= board[this.position];
+        }
+        console.log("balance",this.money);
+    }
 }
 const player1_button = document.getElementById("player-1");
 const player2_button = document.getElementById("player-2");
 console.log(player1_button);
 console.log(player2_button);
 
-player1_button.addEventListener('click',rolldice_1);
-//player1_button.addEventListener('click',myFunction);
-player2_button.addEventListener('click',rolldice_2);
-
-var player1 = ["A",0,1000];
-var player2 = ["B",0,1000];
-
-function rolldice_1()
+var player1 = new player("A",0,1000);
+var player2 = new player("B",0,1000);
+var player_image_1 = "assets/yellowcone.jfif";
+var player_image_2 = "assets/redcone.jfif";
+player1_button.addEventListener('click',function()
 {
-    let position = Math.floor(Math.random()*6)+1;
-    console.log(position);
-    changePosition1(player1[1],position);
-}
-var count = 0;
-function changePosition1(old,newPos)
+    player1.rollDice(player_image_1)
+},false);
+player2_button.addEventListener('click',function()
 {
-    count++;
-    if(count>1)
-    {
-        var elem=document.getElementById('img11');
-        elem.remove();
-    }
-    let updPos = old + newPos;
-    player1[1] = updPos;
-    if(updPos>board.length)
-    updPos %= 15;
-    console.log("Player1 - position", player1[1]);
-    let template = '<img src="assets/redcone.jfif" id="img11" alt="red" style="width: 36px;">';
-    //document.querySelector('#img1').style.display = "";
-    imageArray[updPos].innerHTML += template;
-    changeMoney1(player1[1]);
-}
-function changeMoney1(updPos)
-{
-    if(updPos<board.length)
-    player1[2] = player1[2] - board[updPos];
-    else
-    {
-    updPos %= 15;
-    player1[2] = player1[2] - board[updPos];
-    }
-    console.log("Player1 - amount",player1[2]);
-}
-function rolldice_2()
-{
-    let position = Math.floor(Math.random()*6)+1;
-    console.log(position);
-    changePosition2(player2[1],position);
-}
-function changePosition2(old,newPos)
-{
-    count++;
-    if(count>1)
-    {
-        var elem=document.getElementById('img12');
-        elem.remove();
-    }
-    let updPos = old + newPos;
-    player2[1] = updPos;
-    changeMoney1(player2[1]);
-    console.log("Player1 - position", player1[1]);
-    let template = '<img src="assets/yellowcone.jfif" id="img12" alt="red" style="width: 36px;">';
-    //document.querySelector('#img1').style.display = "";
-    imageArray[updPos].innerHTML += template;
-    console.log("Player2 - position", player2[1]);
-}
-function changeMoney2(updPos)
-{
-    if(updPos<board.length)
-    player2[2] = player2[2] - board[updPos];
-    else
-    {
-    updPos %= 15;
-    player2[2] = player2[2] - board[updPos];
-    }
-    console.log("Player2 - amount", player2[2]);
-}
+    player2.rollDice(player_image_2)
+},false);
